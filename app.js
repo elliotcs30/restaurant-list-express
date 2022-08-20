27a14e9778f95dcd037286f15887320b28001b36
Elliot <elliotcs30@gmail.com>
2022-08-20 14:11:26 +0800

    feat: modify annotation

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// 載入 express 並建構應用程式伺服器
+// require express packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
-// 載入 Restaurant model
+// require Restaurant model
 const Restaurant = require('./models/restaurant')
 
 const app = express()
 
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+// require mongoose
+const mongoose = require('mongoose') 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // setting connect to mongoDB
 
-// 取得資料庫連線狀態
+// get mongoDB connect state
 const db = mongoose.connection
-// 連線異常
+// connect error
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
+// connect success
 db.once('open', () => {
   console.log('mongodb connected!')
 })
@@ -27,15 +28,15 @@ app.set('view engine', 'hbs')
 // setting static files
 app.use(express.static('public'))
 
-// 設定首頁路由
+// routes setting
 app.get('/', (req, res) => {
-  Restaurant.find() // 取出 Restaurant model 裡面所有的資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
+  Restaurant.find() // get Restaurant model all data
+    .lean() //  Mongoose Model object send clear JavaScript data array
+    .then(restaurants => res.render('index', { restaurants })) // mongoDB send data to index template
+    .catch(error => console.error(error)) // handle error
 })
 
-// 設定 port 3000
+// start and listen on the Express server
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
