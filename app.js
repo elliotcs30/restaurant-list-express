6dc1987d7ab353d376c2058aa8d8d075f89e79e0
Elliot <elliotcs30@gmail.com>
2022-08-20 14:48:49 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require express packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 // require Restaurant model
 const Restaurant = require('./models/restaurant')
 
@@ -28,12 +29,26 @@ app.set('view engine', 'hbs')
 // setting static files
 app.use(express.static('public'))
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // routes setting
 app.get('/', (req, res) => {
   Restaurant.find() // get Restaurant model all data
     .lean() //  Mongoose Model object send clear JavaScript data array
     .then(restaurants => res.render('index', { restaurants })) // mongoDB send data to index template
     .catch(error => console.error(error)) // handle error
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  // 
+  return Restaurant.create(req.body) // 從 req.body 拿出表單裡的資料,儲存資料到 mongoDB
+    .then(() => res.redirect('/')) // 新增完成後導回首頁 
+    .catch(error => console.log(error))
 })
 
 // start and listen on the Express server
