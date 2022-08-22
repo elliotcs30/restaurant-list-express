2a0a168f9f1de31d3f72151b3db94e16d299a57f
Elliot <elliotcs30@gmail.com>
2022-08-22 23:45:05 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 // 準備引入路由模組
-// 引入 home 模組程式碼
-const home = require('./modules/home')
-// 引入 restaurant 模組程式碼
-const restaurants = require('./modules/restaurants')
+const home = require('./modules/home') // 引入 home 模組程式碼
+const restaurants = require('./modules/restaurants') // 引入 restaurant 模組程式碼
+const users = require('./modules/users') // 引入 users 模組程式碼
+
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 router.use('/', home)
 
 // 匯出路由器
